4bab759e8205deef8dac0700b0050beb2d1e2491
vladjsp <vladjsp@gmail.com>
2023-08-07 12:25:07 +0300

    add header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import SideBar from './components/SideBar';
 import TripsList from './components/TripsList';
+import WeekWeatherBar from './components/WeekWeatherBar';
 import './scss/app.scss';
 
 function App() {
   return (
     <>
-      <Header />
-      <SearchBar />
-      <TripsList />
+      <div className='container'>
+        <Header />
+        <SearchBar />
+        <TripsList />
+        <WeekWeatherBar />
+      </div>
+      <SideBar />
     </>
   );
 }
