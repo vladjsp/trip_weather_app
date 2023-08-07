2eb667b8b4399d8c08097d20514c226b47cb0333
vladjsp <vladjsp@gmail.com>
2023-08-07 14:22:19 +0300

    add tripsList with mock data

---
+++ src/components/AddTrip.tsx
@@ -0,0 +1,10 @@
+const AddTrip = () => {
+  return (
+    <button className='add-trip' type='button'>
+      <p className='plus'>+</p>
+      <p>Add trip</p>
+    </button>
+  );
+};
+
+export default AddTrip;
--- src/components/TripsList.tsx
+++ src/components/TripsList.tsx
@@ -1,5 +1,16 @@
+import trips from '../data/mock.json';
+import AddTrip from './AddTrip';
+import TripCard from './TripCard';
+
 const TripsList = () => {
-  return <div>TripsList</div>;
+  return (
+    <div className='trips-list'>
+      {trips.map((trip) => (
+        <TripCard key={trip.id} img={trip.image} title={trip.city} dates={trip.startDate} />
+      ))}
+      <AddTrip />
+    </div>
+  );
 };
 
 export default TripsList;
